e00ae3b2d2cd169ffe9e41b9ab6ec2955d2a9168
Michael Wrock <MichaelA.Wrock@gmail.com>
2023-08-28 18:30:32 -0400

    integrate with TensorFlow

--- Sketcher/Screenshot2Sketch.py
+++ Sketcher/Screenshot2Sketch.py
@@ -1,4 +1,11 @@
 import random
+import os
+import pandas as pd
+import cv2
+
+from PIL import Image
+
+
 if __package__ is None or __package__ == '':
     from Functions import *
     from COMPONENT_LEVELS import COMPONENT_LEVELS
@@ -6,6 +13,16 @@ if __package__ is None or __package__ == '':
 else:
     from .Functions import *
     from .COMPONENT_LEVELS import COMPONENT_LEVELS
+
+def insertAnnotation(filename, xmin, ymin, xmax, ymax, clazz):
+    return (
+        filename,
+        clazz,
+        int(xmin),
+        int(ymin),
+        int(xmax),
+        int(ymax),
+    )
 
 def screenshot2Sketch(imagePath, outputPath, fileName):
     
@@ -28,12 +45,20 @@ def screenshot2Sketch(imagePath, outputPath, fileName):
 
     lvl = 1
     bold = False
+    xml_list = []
     for level in objects:
         for element in level:
             key = list(element.keys())[0]
-            x, y, w, h = element[key]
-            insertSketch(sketchImage, key, (x, y, w, h), bold)
+            xmin, ymin, xmax, ymax = element[key]
+            insertSketch(sketchImage, key, (xmin, ymin, xmax, ymax), bold)
+            xml_list.append(insertAnnotation(fileName + '.jpg', xmin, ymin, xmax, ymax, key))
         lvl += 1
         
     cv2.imwrite(outputPath + fileName + '.jpg', sketchImage)
+    column_name = ['filename', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    if os.path.isfile(outputPath + 'labels.csv'):
+        xml_df.to_csv(outputPath + 'labels.csv', mode='a', index=None, header=False)
+    else:
+        xml_df.to_csv(outputPath + 'labels.csv', index=None)
     print('done:', fileName)
